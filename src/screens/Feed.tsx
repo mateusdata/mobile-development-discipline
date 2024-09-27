1dd35b18a6c02e0668c2c49ecd65f10b1c6ff15e
Mateus Santos <mateuspele2015@gmail.com>
2024-09-27 13:29:01 -0300

    feat: added new features

--- src/screens/Feed.tsx
+++ src/screens/Feed.tsx
@@ -1,17 +1,14 @@
 import uniqolor from 'uniqolor';
-
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet, Image, ScrollView, Dimensions, Pressable, Alert, Share } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Dimensions, Pressable, Share, Platform } from 'react-native';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import Feather from '@expo/vector-icons/Feather';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { ContextSheet } from '../context/BottomSheetContex';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import HeaderFeed from '../components/HeaderFeed';
 import ButtonAddPost from '../components/ButtonAddePost';
 import BottomSheet from '../components/BottomSheet';
-import { ActivityIndicator, Avatar, Button, IconButton } from 'react-native-paper';
+import { Avatar, IconButton } from 'react-native-paper';
 import { api } from '../config/Api';
 import { AuthContext } from '../context/AuthContext';
 import { FormatPost } from '../interfaces';
@@ -19,77 +16,52 @@ import dayjs from 'dayjs';
 import { colorPrimary } from '../constants/constants';
 import CommentsBottomSheet from '../components/CommentsBottomSheet';
 import LoadingComponent from '../components/LoadingComponent';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import ConfettiCannon from 'react-native-confetti-cannon';
 
 const FeedScreen = ({ navigation }: any) => {
-  const { openBottomSheet } = useContext(ContextSheet)
+  const { openBottomSheet } = useContext(ContextSheet);
   const { height, width } = Dimensions.get('window');
   const insets = useSafeAreaInsets();
-  const { user, setWelcome, welcome } = useContext(AuthContext)
-  const [posts, setPosts] = useState<FormatPost[]>([])
-  const [currentPost, setCurrentPost] = useState<FormatPost>({} as FormatPost)
+  const { user, setWelcome, welcome } = useContext(AuthContext);
+  const [posts, setPosts] = useState<FormatPost[]>([]);
+  const [currentPost, setCurrentPost] = useState<FormatPost>({} as FormatPost);
   const [showConfetti, setShowConfetti] = useState(false);
-
-
-  const [data, setData] = useState<any>(null);
-  const [socket, setSocket] = useState<WebSocket | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      fetchData();
-
-    }, 5000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
+  const [page, setPage] = useState<number>(1);
+  const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
 
   useEffect(() => {
-    setTimeout(() => {
-      if (welcome) {
-        setShowConfetti(true)
-        setTimeout(() => {
-          setShowConfetti(false)
-          openBottomSheet("FeedScreenWelcome")
-          setWelcome(false)
-        }, 5000);
+    fetchData();
+  }, [page]);
 
-      }
-
-    }, 5000);
-  }, []);
+  useEffect(() => {
+    if (welcome) {
+      setShowConfetti(true);
+      setTimeout(() => {
+        setShowConfetti(false);
+        openBottomSheet("FeedScreenWelcome");
+        setWelcome(false);
+      }, 5000);
+    }
+  }, [welcome]);
 
   async function fetchData() {
     try {
-      setTimeout(async () => {
-        const response = await api.get("/posts");
-        //  console.log(response.data);
-        setPosts(response.data)
-        setLoading(false)
-
-      }, 50);
+      const response = await api.get(`/posts?page=${page}`);
+      setPosts(prevPosts => [...prevPosts, ...response.data]);
+      setLoading(false);
+      setIsRefreshing(false);
     } catch (error) {
-      alert("Erro ao buscar as postagens")
-      setLoading(false)
+      alert("Erro ao buscar as postagens");
+      setLoading(false);
+      setIsRefreshing(false);
     }
   }
-  async function handleLike(item: any) {
 
+  async function handleLike(item: any) {    
   }
 
-
-
-
-  function openCommentsBottomSheet(post: FormatPost) {
-    return
-    /*setCurrentPost(post);
-    if (!!post.comments.length) {
-      openBottomSheet('CommentsBottomSheet');
-      return
-    }*/
+  function openCommentsBottomSheet(post: FormatPost) {    
   }
 
   async function sharePost(post: FormatPost) {
@@ -98,36 +70,28 @@ const FeedScreen = ({ navigation }: any) => {
         title: `Olá estou compartilhando um post de ${post?.user_login}`,
         message: post.message,
       });
-    } catch (error) {
-
+    } catch (error) {      
     }
   }
 
-  if (loading) {
-    return <LoadingComponent />
-  }
   function generationColor(id: number) {
     const colors = [
-      "#FF0000", // Vermelho
-      "green", // Verde
-      "#0000FF", // Azul
-      "orange", // Amarelo
-      "#FF4500", // Laranja Forte
-      "#800080", // Roxo
-      "#00CED1", // Ciano Forte
-      "#FF1493", // Rosa Forte
-      "#696969", // Cinza Escuro
-      "#000000"  // Preto
+    "#FF0000", 
+    "green", 
+    "#0000FF", 
+    "orange", 
+    "#FF4500", 
+    "#800080", 
+    "#00CED1", 
+    "#FF1493", 
+    "#696969", 
+    "#000000"  
     ];
-
-
     return colors[id % colors.length];
   }
 
-  const userUrl = "https://media.istockphoto.com/id/1490373886/pt/foto/natural-beauty.webp?b=1&s=170667a&w=0&k=20&c=JZOyXoFl8iPMqtOySv82u4miOI5NIJGLxryugT12VSg="
-
   const renderPost = ({ item, index }: { item: FormatPost, index: any }) => (
-    <View key={index} style={styles.postContainer}>
+    <View key={index} style={[styles.postContainer, { borderBottomWidth: Platform.OS === "ios" ? 0.5 : 0.2 }]}>
       <View style={{ width: "90%", flexDirection: "row" }}>
         <Pressable onPress={() => navigation.navigate("Profile", {
           profile: {
@@ -135,23 +99,19 @@ const FeedScreen = ({ navigation }: any) => {
             name: item.user_login
           }
         })} style={styles.containerAvatar}>
-
           <Avatar.Text
             color='white' style={{ backgroundColor: generationColor(item.id) }}
             size={35}
             label={item.user_login[0]?.toUpperCase()}
           />
-
         </Pressable>
-
         <View style={styles.postDescription}>
-
           <View style={styles.containerUserTitle}>
             <View style={{ flexDirection: "row", gap: 4, justifyContent: 'center', alignItems: "center" }}>
               <Pressable onPress={() => navigation.navigate("Profile", {
                 profile: {
-                  login: "mateus",
-                  name: "mateus"
+                  login: item?.user_login,
+                  name: item?.user_login
                 }
               })}>
                 <Text numberOfLines={50000} ellipsizeMode="tail" style={styles.postUser}>{item.user_login}</Text>
@@ -160,43 +120,48 @@ const FeedScreen = ({ navigation }: any) => {
                 {dayjs(item.updated_at).format("hh:mm")}
               </Text>
             </View>
-            <Pressable onPress={() => { { } }} android_ripple={{ color: colorPrimary, borderless: true }}>
+            <Pressable onPress={() => { }} android_ripple={{ color: colorPrimary, borderless: true }}>
               <SimpleLineIcons name="options" size={16} color="#c4c4c4" />
             </Pressable>
           </View>
           <Text numberOfLines={50000} ellipsizeMode="tail" style={styles.postMessage}>{item.message}</Text>
-
           <View style={{ flexDirection: "row", marginTop: 12, gap: 8, width: "50%", right: 15 }}>
-
-            <Pressable onPress={() => handleLike(item)} style={{ flexDirection: "row", alignItems: "center", }}>
-
+            <Pressable onPress={() => handleLike(item)} style={{ flexDirection: "row", alignItems: "center" }}>
               {false ?
                 <IconButton icon="heart" iconColor='red' size={22} /> :
                 <IconButton icon="heart-outline" size={22} />
               }
-
               <Text>{""}</Text>
             </Pressable>
-
             <Pressable onPress={() => openCommentsBottomSheet(item)} style={{ flexDirection: "row", alignItems: "center", gap: 12 }}>
               <AntDesign name="message1" size={22} color="black" />
               <Text>{""} </Text>
             </Pressable>
-
-
             <Pressable onPress={() => sharePost(item)} style={{ flexDirection: "row", alignItems: "center", gap: 12, right: 20 }}>
               <IconButton icon="share" size={22} />
             </Pressable>
           </View>
         </View>
-
       </View>
     </View>
   );
 
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleRefresh = () => {
+    setIsRefreshing(true);
+    setPosts([]);
+    setPage(1);
+  };
+
+  if (loading) {
+    return <LoadingComponent />;
+  }
+
   return (
     <SafeAreaProvider>
-
       <ButtonAddPost />
       <CommentsBottomSheet currentPost={currentPost} />
       <View style={[styles.container, {
@@ -205,20 +170,20 @@ const FeedScreen = ({ navigation }: any) => {
         paddingLeft: insets.left,
         paddingRight: insets.right,
       }]}>
-
-
         <FlatList
           ListHeaderComponent={<HeaderFeed />}
           data={posts}
           renderItem={renderPost}
           keyExtractor={(item) => item.id.toString()}
           contentContainerStyle={styles.feedList}
+          onEndReached={handleLoadMore}
+          onEndReachedThreshold={0.5}
+          refreshing={isRefreshing}
+          onRefresh={handleRefresh}
         />
-
         <BottomSheet id="FeedScreenWelcome" snapPoints={[height < 700 ? 45 : 35]}>
           <View style={{ padding: 20, alignItems: 'center' }}>
             <>
-
               <Avatar.Text
                 color='white'
                 style={{ backgroundColor: uniqolor.random().color }}
@@ -234,7 +199,6 @@ const FeedScreen = ({ navigation }: any) => {
             </>
           </View>
         </BottomSheet>
-
         {showConfetti && (
           <ConfettiCannon
             count={200}
@@ -242,7 +206,6 @@ const FeedScreen = ({ navigation }: any) => {
             fadeOut={true}
           />
         )}
-
       </View>
     </SafeAreaProvider>
   );
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, Share } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Share } from 'react-native';
 import { Avatar } from 'react-native-paper';
 import uniqolor from 'uniqolor';
 import { api } from '../config/Api';
@@ -8,9 +8,8 @@ import LoadingComponent from '../components/LoadingComponent';
 import { playSound } from '../utils/playSound';
 
 interface FormarProfile {
-  login: string
-  name: string
-
+  login: string;
+  name: string;
 }
 
 interface Follow {
@@ -23,85 +22,84 @@ interface Follow {
 }
 
 export default function Profile({ route }: { route: { params: { profile: FormarProfile } } }) {
-  const [isFollowing, setIsFollowing] = useState(false)
-  const [loading, setLoading] = useState(true)
-  const [followers, setFollowers] = useState('')
-  const { user } = useContext(AuthContext)
-  const { profile } = route?.params
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [followers, setFollowers] = useState<number | null>(null);
+  const { user } = useContext(AuthContext);
+  const { profile } = route?.params;
 
   async function fetchData() {
     try {
-      const response = await api.get(`users/${profile?.login}/followers`)
-      console.log("Meus sequidores");
-      const isMatch = response?.data?.find((item: Follow) => (item.follower_login === user?.user_login))
-      console.log("Já seque", isMatch);
+      const response = await api.get(`users/${profile?.login}/followers`);
+      const isMatch = response?.data?.find((item: Follow) => item.follower_login === user?.user_login);
       if (isMatch) {
-        setFollowers(isMatch.follower_id)
-        setIsFollowing(true)
+        setFollowers(isMatch.follower_id);
+        setIsFollowing(true);
       }
-      console.log(response.data)
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
-      console.log("OCoreu um error");
-      console.log(error);
-      setLoading(false)
-
+      console.log("Ocorreu um erro", error);
+      setLoading(false);
     }
   }
+
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
 
   async function followersUser() {
     try {
       if (isFollowing) {
-        const deseguir = await api.delete(`users/${profile.login}/followers/${followers}`);
-        playSound()
+        await api.delete(`users/${profile.login}/followers/${followers}`);
+        playSound();
         setIsFollowing(false);
-        return
+        setFollowers(null);
+      } else {
+        const response = await api.post(`users/${profile?.login}/followers`);
+        setIsFollowing(true);
+        setFollowers(response.data.id);
+        playSound();
       }
-      const seguir = await api.post(`users/${profile?.login}/followers`);
-      setIsFollowing(true)
-      playSound()
     } catch (error: any) {
-
       if (error?.response?.status === 422) {
-        api.delete(`users/${profile.login}/followers/${followers}`)
-        return alert("Ops? muitas tentativas, sai da pagina e tente novamente")
+        await api.delete(`users/${profile.login}/followers/${followers}`);
+        
       }
-      setIsFollowing(false)
+      setIsFollowing(false);
     }
   }
+
   async function sharePost() {
     try {
       await Share.share({
-        title: `Olá estou compartilhando um post de ${profile?.login}`,
-        message: `Olá estou capartilhaddo este perfil do papacapim @${profile?.login}`,
+        title: `Olá, estou compartilhando um post de ${profile?.login}`,
+        message: `Olá, estou compartilhando este perfil do papacapim @${profile?.login}`,
       });
     } catch (error) {
-
+      console.log("Erro ao compartilhar", error);
     }
   }
 
   if (loading) {
-    return <LoadingComponent />
+    return <LoadingComponent />;
   }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Avatar.Text
           color='white'
-          style={{ backgroundColor: uniqolor.random().color }}
+          style={{ backgroundColor: "green" }}
           size={80}
           label={profile?.name ? profile?.name[0]?.toUpperCase() : profile?.login[0]?.toUpperCase()}
         />
-        <Text style={styles.name}>{profile?.name} </Text>
+        <Text style={styles.name}>{profile?.name}</Text>
         <Text style={styles.username}>{"@" + profile?.login}</Text>
       </View>
 
       <View style={styles.stats}>
         <View style={styles.stat}>
-          <Text style={styles.statNumber}>123  </Text>
+          <Text style={styles.statNumber}>123</Text>
           <Text>Postagens</Text>
         </View>
         <View style={styles.stat}>
@@ -115,7 +113,7 @@ export default function Profile({ route }: { route: { params: { profile: FormarP
       </View>
       <View style={styles.buttonsContainer}>
         <TouchableOpacity onPress={followersUser} style={[styles.followButton, { backgroundColor: isFollowing ? "black" : "gray" }]}>
-          <Text style={styles.buttonText}>{`${isFollowing ? "Seguindo" : "Seguir"}`}</Text>
+          <Text style={styles.buttonText}>{isFollowing ? "Seguindo" : "Seguir"}</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={sharePost} style={styles.mentionButton}>
           <Text style={styles.buttonText}>Compartilhar</Text>
@@ -134,12 +132,6 @@ const styles = StyleSheet.create({
   header: {
     alignItems: 'center',
     marginBottom: 20,
-  },
-  profileImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    marginBottom: 16,
   },
   name: {
     fontSize: 24,
